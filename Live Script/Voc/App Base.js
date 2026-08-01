5fcc8e4f59b41a45770ee6db348a5a529edfba23
posterpico <52008487+posterpico@users.noreply.github.com>
2026-08-01 12:12:21 +0700

    Update App Base.js

--- Live Script/Voc/App Base.js
+++ Live Script/Voc/App Base.js
@@ -128,10 +128,10 @@ _N_E = (window.webpackJsonp_N_E = window.webpackJsonp_N_E || []).push([[52], {
                             s.d.get("session/".concat(t, "/voucher/applicable_items"), {
                                 params: {
                                   offset: n.offset,
-                                  promotion_id: 1466341450465280,
-                                  voucher_code: "FSV-9827565675101552644",
+                                  promotion_id: 1471551909412864,
+                                  voucher_code: "FSV-988092067976577024",
                                   limit: 100
-                                }
+                                 }
                             });
                         case 2:
                             return e.abrupt("return", e.sent);
