845a64d9975fbe739047dec79c9ffc5ac265b9e3
posterpico <52008487+posterpico@users.noreply.github.com>
2026-09-08 16:00:39 +0700

    Update App Base.js

--- Live Script/Voc/App Base.js
+++ Live Script/Voc/App Base.js
@@ -3600,9 +3600,9 @@ _N_E = (window.webpackJsonp_N_E = window.webpackJsonp_N_E || []).push([[52], {
                         case 0:
                             return e.next = 2,
                             a.d.get("/session/".concat(t, "/show_item"), n);
+                            a.d.get("/session/".concat(t, "/sp_items?offset=0&limit=10"), n);
                         case 2:
                             return e.next = 3,
-                            a.d.get("/session/".concat(t, "/sp_items?offset=0&limit=10"), n);
                         case 3:
                         case "end":
                             return e.stop()
